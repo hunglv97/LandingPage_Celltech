7ac903c4192357fa8d852965b15132cd167036aa
thaind <nguyenduythai12@gmail.com>
2023-05-08 01:02:22 +0700

    chore: install swiper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,30 @@
 import { Component } from '@angular/core';
 
+// import Swiper core and required components
+import SwiperCore , {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Virtual,
+  Zoom,
+  Autoplay,
+  Thumbs,
+  Controller,
+} from 'swiper';
+
+// install Swiper components
+SwiperCore.use([
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Virtual,
+  Zoom,
+  Autoplay,
+  Thumbs,
+  Controller
+]);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +32,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LandingPage_Celltech';
+  thumbsSwiper: any;
 }
